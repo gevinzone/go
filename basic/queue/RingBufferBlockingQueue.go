9b8d287d9023a5cf2b2dbb99bcd8eb8770efea5b
gevin <flyhigher139@gmail.com>
2023-02-07 08:52:59 +0800

    ring buffer blocking queue

--- basic/queue/RingBufferBlockingQueue.go
+++ basic/queue/RingBufferBlockingQueue.go
@@ -1,7 +1,6 @@
 package queue
 
 import (
-	"context"
 	"sync"
 )
 
@@ -32,70 +31,34 @@ func (r *RingBufferBlockingQueue[T]) IsEmpty() bool {
 	return r.q.IsEmpty()
 }
 
-func (r *RingBufferBlockingQueue[T]) Enqueue(ctx context.Context, val T) error {
-	select {
-	case <-ctx.Done():
-		return ctx.Err()
-	default:
-		return r.enqueue(ctx, val)
-	}
-}
-
-func (r *RingBufferBlockingQueue[T]) enqueue(ctx context.Context, val T) error {
-	select {
-	case <-ctx.Done():
-		return ctx.Err()
-	default:
-		for r.IsFull() {
-			<-r.queueNotFullSignal
-		}
-		r.m.Lock()
-		for r.q.IsFull() {
-			r.m.Unlock()
-			<-r.queueNotFullSignal
-			r.m.Lock()
-		}
-		err := r.q.Enqueue(val)
-		if len(r.queueNotEmptySignal) == 0 {
-			r.queueNotEmptySignal <- struct{}{}
-		}
+func (r *RingBufferBlockingQueue[T]) Enqueue(val T) error {
+	r.m.Lock()
+	for r.q.IsFull() {
 		r.m.Unlock()
-		return err
-	}
-}
-
-func (r *RingBufferBlockingQueue[T]) Dequeue(ctx context.Context) (T, error) {
-	select {
-	case <-ctx.Done():
-		var t T
-		return t, ctx.Err()
-	default:
-		return r.dequeue(ctx)
-	}
-}
-
-func (r *RingBufferBlockingQueue[T]) dequeue(ctx context.Context) (T, error) {
-	select {
-	case <-ctx.Done():
-		var t T
-		return t, ctx.Err()
-	default:
-		for r.IsEmpty() {
-			<-r.queueNotEmptySignal
-		}
+		<-r.queueNotFullSignal
 		r.m.Lock()
-		for r.q.IsEmpty() {
-			r.m.Unlock()
-			<-r.queueNotEmptySignal
-			r.m.Lock()
-		}
-		t, err := r.q.Dequeue()
-		if len(r.queueNotFullSignal) == 0 {
-			r.queueNotFullSignal <- struct{}{}
-		}
-		r.m.Unlock()
-		return t, err
 	}
+	err := r.q.Enqueue(val)
+	if len(r.queueNotEmptySignal) == 0 {
+		r.queueNotEmptySignal <- struct{}{}
+	}
+	r.m.Unlock()
+	return err
+}
+
+func (r *RingBufferBlockingQueue[T]) Dequeue() (T, error) {
+	r.m.Lock()
+	for r.q.IsEmpty() {
+		r.m.Unlock()
+		<-r.queueNotEmptySignal
+		r.m.Lock()
+	}
+	val, err := r.q.Dequeue()
+	if len(r.queueNotFullSignal) == 0 {
+		r.queueNotFullSignal <- struct{}{}
+	}
+	r.m.Unlock()
+	return val, err
 }
 
 func (r *RingBufferBlockingQueue[T]) Peek() (T, error) {
